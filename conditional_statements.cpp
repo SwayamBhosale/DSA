6f6dcd383de59a07800e879d79372858384d70c8
Swayam Bhosale <swayamb7@gmail.com>
2026-01-16 12:05:03 +0530

    Conditional Statements

---
+++ conditional_statements.cpp
@@ -0,0 +1,17 @@
+//if-else statement
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    cout<<"What is your name? "<<endl;
+    cin>>name;
+    
+    if(name=="Neha"){
+        cout<<"You are a retard"<<endl;
+    }
+    else{
+        cout<<"You are not a retard"<<endl;
+    }
+    return 0;
+}
