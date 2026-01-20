20b50f26a0aea8c4f9c640037d5047882a5aef15
Swayam Bhosale <swayamb7@gmail.com>
2026-01-20 13:16:19 +0530

    Test commit

--- patterns.cpp
+++ patterns.cpp
@@ -2,18 +2,18 @@
 #include <iostream>
 using namespace std;
 
-// int main() {
-//     int n = 4;
-//     for (int i=0; i<n; i++)
-//     {
-//         for (int j=0; j<n; j++)
-//         {
-//         cout<<"* ";
-//         }
-//     cout<<endl;
-//     }
+int main() {
+    int n = 4;
+    for (int i=0; i<n; i++)
+    {
+        for (int j=0; j<n; j++)
+        {
+        cout<<"* ";
+        }
+    cout<<endl;
+    }
     
-// }
+}
 
 int main() {
     int n = 4;
